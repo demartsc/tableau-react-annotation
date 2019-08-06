aceeea0de61ffab80b577fc385126c1b304fdcbb
Chris DeMartini <chris@datablick.com>
2019-08-06 00:02:40 -0700

    Updates

---
+++ precache-manifest.06eebd0c3595d27ba84775eb8fa98cf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2a8dd894981cbbe7678f9f1622c082c",
+    "url": "/tableau-react-annotation/static/media/logo.e2a8dd89.png"
+  },
+  {
+    "revision": "4533e587ba57915367e5dab7b59b905d",
+    "url": "/tableau-react-annotation/static/media/dblogo.4533e587.png"
+  },
+  {
+    "revision": "c484e4a27916d31874d0",
+    "url": "/tableau-react-annotation/static/js/runtime~main.c484e4a2.js"
+  },
+  {
+    "revision": "1ef21049ac94319d7873",
+    "url": "/tableau-react-annotation/static/js/main.1ef21049.chunk.js"
+  },
+  {
+    "revision": "98b8c243df12b9e987c3",
+    "url": "/tableau-react-annotation/static/js/2.98b8c243.chunk.js"
+  },
+  {
+    "revision": "1ef21049ac94319d7873",
+    "url": "/tableau-react-annotation/static/css/main.e92bbc39.chunk.css"
+  },
+  {
+    "revision": "7155e1e770571b1761baad4e3d034af5",
+    "url": "/tableau-react-annotation/index.html"
+  }
+];
