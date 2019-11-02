ea58fc98482a363c7cd823cd93de9b6f435ded44
Chris DeMartini <chris@datablick.com>
2019-11-02 15:51:45 -0700

    fix: clean up extra files and add licensing to js files across repo

--- src/extension_assets/splash/index.js
+++ src/extension_assets/splash/index.js
@@ -1,3 +1,23 @@
+// Copyright (c) 2019 Chris DeMartini
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 import React from 'react';
 import '../styles/SplashScreen.css';
 
@@ -71,7 +91,7 @@ const CustomSplash = (props) => (
         >
           React Annotation
         </a>{" "}
-        Made with <img alt="A picture of a heart" src="img/heart.png" /> by
+        Made with <img alt="A heart" src="img/heart.png" /> by
         <a 
           href="http://www.susielu.com"
           target="_blank"
--- src/extension_assets/viz/index.js
+++ src/extension_assets/viz/index.js
@@ -1,4 +1,24 @@
-import React, { useContext, useEffect, useState } from 'react';
+// Copyright (c) 2019 Chris DeMartini
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import React, { useContext, useState } from 'react';
 import _ from 'lodash';
 import uuid from 'uuid';
 import { 
@@ -19,7 +39,6 @@ import {
   curveNatural, 
   curveBasis
 } from 'd3-shape'
-import TypesUI  from '../components/annotations/Types';
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import getMuiTheme from "material-ui/styles/getMuiTheme";
 import { muiTheme } from "../components/annotations/Theme";
@@ -584,7 +603,6 @@ const Viz = (props) => {
 
   return (
     <MuiThemeProvider muiTheme={getMuiTheme(muiTheme)}>
-        {/* <TypesUI /> */}
       <div 
         className="Annotation-Div"
         onMouseEnter={() => { setIconViewState(true)}}
